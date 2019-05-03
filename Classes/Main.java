ed51736f0f4ef2a38c7ca82e6bdbc18133931449
Spencer White <swhite215@live.com>
2019-05-03 10:53:59 -0400

    example class instantiation

--- Classes/Main.java
+++ Classes/Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
+        //Getters and Setters
         Car porsche = new Car();
-        Car holden = new Car();
 
         porsche.setColor("Green");
         porsche.setDoors(4);
@@ -15,5 +15,14 @@ public class Main {
         System.out.println("Porsche engine is " + porsche.getEngine());
         System.out.println("Porsche model is " + porsche.getModel());
         System.out.println("Porsche number of wheels is " + porsche.getWheels());
+
+        //Constructor
+        Car bentley = new Car(4, 4, "continental", "v8", "silver");
+
+        System.out.println("Bentley color is " + bentley.getColor());
+        System.out.println("Bentley number of doors is " + bentley.getDoors());
+        System.out.println("Bentley engine is " + bentley.getEngine());
+        System.out.println("Bentley model is " + bentley.getModel());
+        System.out.println("Bentley number of wheels is " + bentley.getWheels());
     }
 }
