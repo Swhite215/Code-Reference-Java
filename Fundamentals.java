21ea4f0405eb3c7a61df812fb17ead94149f8591
Spencer <swhite215@live.com>
2019-05-09 08:59:18 -0400

    notes on reference types

--- Fundamentals.java
+++ Fundamentals.java
@@ -2,46 +2,56 @@ public class Fundamentals {
 
     public static void main(String[] args) {
 
-        //Variables
+        // Variables
         int myAge = 26;
         String firstName = "Spencer";
         boolean engaged = true;
         double height = 5.11;
 
-        //Primitive Types
+        // Primitive Types - Holds a Value
         int myMinValue = -2_147_483_648; // 4 Bytes
         int myMaxValue = 2_147_483_647;
 
-        byte myMinByteValue = -127; //Quarter of an Int, 1 Byte
+        byte myMinByteValue = -127; // Quarter of an Int, 1 Byte
         byte myMaxByteValue = 127;
 
-        short myMinShort = -32768; //Half an Int, 2 Bytes
+        short myMinShort = -32768; // Half an Int, 2 Bytes
         short myMaxShort = 32767;
 
-        long myMaxLongValue = 2^63L; //Double an Int, 8 Bytes
-        long myMinLongValue = -2^63L;
+        long myMaxLongValue = 2 ^ 63L; // Double an Int, 8 Bytes
+        long myMinLongValue = -2 ^ 63L;
 
-        float myFloat = 5.2F; //Seven Digits of Precision, 4 Bytes
+        float myFloat = 5.2F; // Seven Digits of Precision, 4 Bytes
 
-        double myDouble = 5.25D;; //Sixteen Digits of Precision, 8 Bytes
+        double myDouble = 5.25D;
+        ; // Sixteen Digits of Precision, 8 Bytes
 
-        char myChar = 'S'; //Half an Int, 2 Bytes
+        char myChar = 'S'; // Half an Int, 2 Bytes
         char myUnicodeChar = '\u00A9';
 
         boolean myTrueBool = true;
         boolean myFalseBool = false;
         boolean invertBool = !true;
 
-        //Casting
-        byte castByte = (byte)(myMaxByteValue / 2);
-        short castShort = (short)(myMaxShort / 2);
-        float castFloat = (float)(myFloat / 1.2);
+        // Reference Types - Holds address to object in memory, use new for unique...
+        int[] myIntArray = new int[5];
+        int[] anotherIntArray = myIntArray;
 
-        //Operators
+        System.out.println(Array.toString(myIntArray));
+        System.out.println(Array.toString(anotherIntArray));
+
+        anotherIntArray[0] = 10; // This will change myIntArray[0]
+
+        // Casting
+        byte castByte = (byte) (myMaxByteValue / 2);
+        short castShort = (short) (myMaxShort / 2);
+        float castFloat = (float) (myFloat / 1.2);
+
+        // Operators
         int sum = myAge + 1;
         int difference = myAge - 1;
         int product = myAge * 2;
-        int quotient = (int)(myAge / height);
+        int quotient = (int) (myAge / height);
         int modulus = myAge % 2;
         int postIncrement = myAge++;
         int preIncrement = ++myAge;
