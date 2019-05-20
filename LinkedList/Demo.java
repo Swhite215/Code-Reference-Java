ae54bd0acdc5505c465108a92c350a2fd9e03aca
Spencer <swhite215@live.com>
2019-05-20 09:18:57 -0400

    example with menu using linked list methods

--- LinkedList/Demo.java
+++ LinkedList/Demo.java
@@ -69,4 +69,72 @@ public class Demo {
         stringListIterator.add(newCity);
         return true;
     }
+
+    private static void visit(LinkedList cities) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward = true;
+
+        ListIterator<String> listIterator = cities.listIterator();
+
+        if (cities.isEmpty()) {
+            System.out.println("No cities in the itenerary.");
+            return;
+        } else {
+            System.out.println("Now visiting: " + listIterator.next());
+        }
+
+        while (!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+            case 0:
+                System.out.println("Holiday (Vacation) over");
+                quit = true;
+                break;
+            case 1:
+                if (!goignForward) {
+                    if (listIterator.hasNext()) {
+                        listIterator.next();
+                    }
+
+                    goingForward = true;
+                }
+                if (listIterator.hasNext()) {
+                    System.out.println("Now visiting: " + listIterator.next());
+                } else {
+                    System.out.println("Reached the end of the list.");
+                    goingForward = false;
+                }
+                break;
+            case 2:
+                if (goingForward) {
+                    if (listIterator.hasPrevious()) {
+                        listIterator.previous();
+                    }
+                    goingForward = false;
+                }
+                if (listIterator.hasPrevious()) {
+                    System.out.println("Now visiting: " + listIterator.previous());
+                } else {
+                    System.out.println("We are at the start of the list.");
+                    goingForward = true;
+                }
+                break;
+            case 3:
+                printMenu();
+                break;
+            default:
+                System.out.println("Please enter 0, 1, or 2");
+                printMenu();
+                break;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Available actions: \n press ");
+        System.out.println("0 - Quit\n " + "1 - Go to Next City\n" + "2 - Go to Previous City\n" + "3 - Print Menu Again")
+    }
 }
