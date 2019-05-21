67261857616e58744c38d89f931f700fe9415aee
Spencer <swhite215@live.com>
2019-05-21 09:08:42 -0400

    example class that implements interface

---
+++ Interfaces/MobilePhone.java
@@ -0,0 +1,50 @@
+public class MobilePhone implements ITelephone {
+    private int myNumber;
+    private boolean isRinging;
+    private boolean isOn = false;
+
+    public MobilePhone(int myNumber) {
+        this.myNumber = myNumber;
+    }
+
+    @Override
+    public void powerOn() {
+        isOn = true;
+        System.out.println("Mobile phone powered on.");
+    }
+
+    @Override
+    public void dial(int phoneNumber) {
+        if (isOn) {
+            System.out.println("Dialing: " + phoneNumber);
+        } else {
+            System.out.println("Phone is switched off.");
+        }
+    }
+
+    @Override
+    public void answer() {
+        if (isRinging) {
+            System.out.println("Answering the mobile phone.");
+            isRinging = false;
+        }
+    }
+
+    @Override
+    public boolean callPhone(int phoneNumber) {
+        if (phoneNumber == myNumber && isOn) {
+            isRinging = true;
+            System.out.println("Ring ring...");
+        } else {
+            isRinging = false;
+            System.out.println("Number wrong or phone is not on.");
+        }
+
+        return isRinging;
+    }
+
+    @Override
+    public boolean isRinging() {
+        return isRinging;
+    }
+}
