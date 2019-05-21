43902d916a78ac593dec3337c002a88d02d24bc2
Spencer <swhite215@live.com>
2019-05-21 09:30:13 -0400

    example creating class and inner class

---
+++ InnerClasses/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Gearbox mcLaren = new Gearbox(6);
+        Gearbox.Gear first = mcLaren.new Gear(1, 12.3); // OuterClass.InnerClass
+    }
+}
