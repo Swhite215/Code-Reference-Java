309ab2d6490bcf83acfc12de8db23b25066e69b7
Spencer <swhite215@live.com>
2019-05-23 09:21:52 -0400

    implementing local class within a class

--- InnerClasses/Main.java
+++ InnerClasses/Main.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button buttonPrint = new Button("Print");
+
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
         // Gearbox.Gear first = mcLaren.new Gear(1, 12.3); // OuterClass.InnerClass
@@ -14,5 +19,35 @@ public class Main {
         mcLaren.changeGear(2);
         System.out.println(mcLaren.wheelSpeed(1000));
 
+        class ClickListener implements Button.OnClickListener {
+            public ClickListener() {
+                System.out.println("I've been attacked!");
+            }
+
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        }
+
+        buttonPrint.setOnClickListener(new ClickListener());
+        listen();
+    }
+
+    private static void listen() {
+        boolean quit = false;
+
+        while (!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+            case 0:
+                quit = true;
+                break;
+            case 1:
+                buttonPrint.onClick();
+                break;
+            }
+        }
     }
 }
