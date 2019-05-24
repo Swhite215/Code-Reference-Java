7d4a7b904f346899825659a6101b36c05f5c95f8
Spencer <swhite215@live.com>
2019-05-24 09:20:01 -0400

    removed unnecessary method

--- AbstractClasses/Hawk.java
+++ AbstractClasses/Hawk.java
@@ -2,9 +2,4 @@ public class Hawk extends Bird {
     public Hawk(String name) {
         super(name);
     }
-
-    @Override
-    public void fly() {
-        System.out.println("Flying through the air.");
-    }
 }
