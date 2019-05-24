6373fb80a4a9309e16a3d6ad36c20b928c2691af
Spencer <swhite215@live.com>
2019-05-24 09:14:02 -0400

    using classes that extend abstract classes

---
+++ AbstractClasses/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Shiba Inu");
+        dog.breathe();
+        dog.eat();
+
+        Hawk bird = new Hawk("Laser Wing");
+        bird.breathe();
+        bird.eat();
+    }
+}
