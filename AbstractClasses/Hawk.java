0b2efad7f587a48793ffacc8cfe673fbe4a1edbb
Spencer <swhite215@live.com>
2019-05-24 09:13:47 -0400

    class extending twice extended abstract class

---
+++ AbstractClasses/Hawk.java
@@ -0,0 +1,10 @@
+public class Hawk extends Bird {
+    public Hawk(String name) {
+        super(name);
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Flying through the air.");
+    }
+}
