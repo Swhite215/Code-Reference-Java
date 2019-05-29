f59f4f53ef2059525a491df642edfd4ab654ba42
Spencer <swhite215@live.com>
2019-05-29 08:59:38 -0400

    code using generics and specific types for teams

--- Generics/Main.java
+++ Generics/Main.java
@@ -8,6 +8,21 @@ public class Main {
         // items.add("test"); // Error because of assigned data type
 
         printDoubled(items);
+
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<FootballPlayer> seattleSonics = new Team<>("Seattle Sonics"); // Only accepts football players
+        Team<BaseballPlayer> whiteSox = new Team<>("White Sox"); // Only accepts baseball players
+        Team<SoccerPlayer> seattleSounders = new Team<>("Seattle Sounders"); // Only accepts soccer players
+
+        seattleSonics.addPlayer(joe); // Valid Player for Football Team
+
+        whiteSox.addPlayer(pat); // Valid Player for Baseball Team
+
+        System.out.println(seattleSonics.numPlayers());
+        System.out.println(whiteSox.numPlayers());
     }
 
     private static void printDoubled(ArrayList<Integer> n) {
