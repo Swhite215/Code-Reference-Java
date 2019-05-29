e0e248c775e1a5148c5810de02b4196dc3262b1c
Spencer <swhite215@live.com>
2019-05-29 09:24:01 -0400

    naming conventions for important items in java

---
+++ NamingConventions.java
@@ -0,0 +1,32 @@
+public class NamingConventions {
+
+    // Packages
+    // ---- Always lower case
+    // ---- Name should be unique
+    // ---- Use internet domain name reversed as prefix for the name
+    // ---- Example: java.lang
+
+    // Classes
+    // ---- CamelCase, Nouns, Capital Words
+    // ---- Examples: ArrayList, LinkedList
+
+    // Interfaces
+    // ---- CamelCase, Nouns, Capital Words
+    // ---- Examples: List, Comparable
+
+    // Methods
+    // ---- mixedCase, Verbs
+    // ---- Examples: getName(), addPlayer()
+
+    // Constants
+    // ---- Uppercase, Separate words with underscore_
+    // ---- Examples: MAX_INT, SEVERITY_ERROR
+
+    // Variables
+    // ---- mixedCase meaningful and indicative
+    // ---- Examples: numOfPlayers, name
+
+    // Type Parameters
+    // ---- Single Character, Capital Letters
+    // ---- Examples: E - Element, K - Key, T - Type, V - Value
+}
